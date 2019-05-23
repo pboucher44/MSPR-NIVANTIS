5b4344a3ad9a1ba51a8a98034b04ec58db3c8d21
aloishias <hias.alois@gmail.com>
2019-05-23 13:10:32 +0200

    Add test

---
+++ src/test/java/org/ss/nivantis/nivantisapirest/controller/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package org.ss.nivantis.nivantisapirest.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void getMarco() {
+    }
+
+    @Test
+    public void getTauxDeRemise() {
+    }
+
+    @Test
+    public void getPrixAchatNet() {
+    }
+
+    @Test
+    public void getPrixVentetNet() {
+    }
+
+    @Test
+    public void getCoefficientMultiplicateur() {
+    }
+
+    @Test
+    public void createPharmacie() {
+    }
+}
