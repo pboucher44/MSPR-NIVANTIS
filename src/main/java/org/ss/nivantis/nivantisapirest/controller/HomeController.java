4a5007ad4a52dfae5328194a4e55269e5dadc159
Remi- <remi-castel@hotmail.fr>
2019-06-17 15:27:58 +0200

    API REST refacto

--- src/main/java/org/ss/nivantis/nivantisapirest/controller/HomeController.java
+++ src/main/java/org/ss/nivantis/nivantisapirest/controller/HomeController.java
@@ -1,42 +1,49 @@
 package org.ss.nivantis.nivantisapirest.controller;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.ss.nivantis.nivantisapirest.dao.PharmacieRepository;
 import org.ss.nivantis.nivantisapirest.model.Pharmacie;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.Scanner;
+
 
 @RestController
 public class HomeController {
     @Autowired
     PharmacieRepository pharmacieRepository;
 
-    @RequestMapping("/Marco")
+    @GetMapping("/Marco")
     public String getMarco(){
         return "Polo";
     }
 
-    @RequestMapping("/Math/TauxDeRemise")
+    @GetMapping("/Math/TauxDeRemise")
     public float getTauxDeRemise(@RequestParam("Net") float net, @RequestParam("Brut") float brut ){
+
+
         return ((1-net/brut)*100);
     }
 
-    @RequestMapping("/Math/PrixAchatNet")
+    @GetMapping("/Math/PrixAchatNet")
     public float getPrixAchatNet(@RequestParam("Brut") float brut, @RequestParam("Taux") float taux ){
         return brut*(1-taux);
     }
 
-    @RequestMapping("/Math/PrixVentetNet")
+    @GetMapping("/Math/PrixVentetNet")
     public float getPrixVentetNet(@RequestParam("Net") float net, @RequestParam("Coef") float coef ){
         return net*coef;
     }
 
-    @RequestMapping("/Math/CoefficientMultiplicateur")
+    @GetMapping("/Math/CoefficientMultiplicateur")
     public float getCoefficientMultiplicateur(@RequestParam("Vente") float vente, @RequestParam("Achat") float achat ){
         return vente/achat;
     }
@@ -53,6 +60,24 @@ public class HomeController {
     public Collection<Pharmacie> findAllController()
     {
         return (Collection<Pharmacie>) pharmacieRepository.findAll();
+    }
+
+    @GetMapping("/get/PharmacieProche")
+    public ArrayList findProche(
+            @RequestParam("latitude") float latitude, @RequestParam("longitude") float longitude)
+    {
+
+        ArrayList arraylist = new ArrayList();
+        arraylist.add(pharmacieRepository.findAll());
+        JSONArray jsonarr_1 = new JSONArray(arraylist);
+        return arraylist;
+    }
+
+    @GetMapping("/get/PharmacieId")
+    public Optional<Pharmacie> findById(
+            @RequestParam("Id") long id)
+    {
+        return pharmacieRepository.findById(id);
     }
 
 
--- src/main/java/org/ss/nivantis/nivantisapirest/dao/PharmacieRepository.java
+++ src/main/java/org/ss/nivantis/nivantisapirest/dao/PharmacieRepository.java
@@ -1,9 +1,17 @@
 package org.ss.nivantis.nivantisapirest.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.ss.nivantis.nivantisapirest.model.Pharmacie;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 public interface PharmacieRepository extends CrudRepository<Pharmacie, Long> {
+
+
+
+
 
 
 
