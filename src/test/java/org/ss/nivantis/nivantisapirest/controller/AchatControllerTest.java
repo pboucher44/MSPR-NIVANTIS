1077a9d48b40f981c2029738d26464c1eecae641
pboucher44 <boucher.pierre44400@hotmail.fr>
2019-06-21 19:54:37 +0200

    Merge branch 'master' of https://github.com/pboucher44/MSPR-NIVANTIS
    # Conflicts:
    #	nivantis-apirest.iml
    #	src/main/resources/application.properties
    #	target/classes/org/ss/nivantis/nivantisapirest/DatabaseLoader.class
    #	target/classes/org/ss/nivantis/nivantisapirest/NivantisApirestApplication.class
    #	target/classes/org/ss/nivantis/nivantisapirest/configuration/SwaggerConfig.class
    #	target/classes/org/ss/nivantis/nivantisapirest/controller/ClientController.class
    #	target/classes/org/ss/nivantis/nivantisapirest/controller/DMOController.class
    #	target/classes/org/ss/nivantis/nivantisapirest/controller/HomeController.class
    #	target/classes/org/ss/nivantis/nivantisapirest/controller/PharmacieController.class
    #	target/classes/org/ss/nivantis/nivantisapirest/controller/ProduitController.class
    #	target/classes/org/ss/nivantis/nivantisapirest/dao/AchatRepository.class
    #	target/classes/org/ss/nivantis/nivantisapirest/dao/ClientRepository.class
    #	target/classes/org/ss/nivantis/nivantisapirest/dao/DMORepository.class
    #	target/classes/org/ss/nivantis/nivantisapirest/dao/PharmacieRepository.class
    #	target/classes/org/ss/nivantis/nivantisapirest/dao/ProduitRepository.class
    #	target/classes/org/ss/nivantis/nivantisapirest/model/Achat.class
    #	target/classes/org/ss/nivantis/nivantisapirest/model/Client.class
    #	target/classes/org/ss/nivantis/nivantisapirest/model/DMO.class
    #	target/classes/org/ss/nivantis/nivantisapirest/model/Pharmacie.class
    #	target/classes/org/ss/nivantis/nivantisapirest/model/Produit.class

--- src/test/java/org/ss/nivantis/nivantisapirest/controller/AchatControllerTest.java
+++ src/test/java/org/ss/nivantis/nivantisapirest/controller/AchatControllerTest.java
@@ -1,33 +1,23 @@
 package org.ss.nivantis.nivantisapirest.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.ss.nivantis.nivantisapirest.model.*;
 
 import java.util.ArrayList;
 
-import static com.sun.xml.internal.ws.policy.sourcemodel.wspolicy.XmlToken.Optional;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -43,7 +33,7 @@ public class AchatControllerTest {
     @Test
     public void testCreateAchat() throws Exception {
         DMO monDmo = new DMO("BOUCHER","Pierre","pboucher","pboucher",new ArrayList<Achat>());
-        Pharmacie maPharmacie = new Pharmacie("adresse",87,44444,"2.5","6.1",new ArrayList<Achat>());
+        Pharmacie maPharmacie = new Pharmacie("adresse",87,44444,"2.5","6.1","maPharmacie",new ArrayList<Achat>());
         maPharmacie.setId(0L);
         Client monClient = new Client("BOUCHER","Pierre","mail@mail.fr","0654478956",new ArrayList<Achat>());
         monClient.setId(0L);
@@ -65,7 +55,7 @@ public class AchatControllerTest {
     @Test
     public void testFindByIdAchat() throws Exception {
         DMO monDmo = new DMO("BOUCHER","Pierre","pboucher","pboucher",new ArrayList<Achat>());
-        Pharmacie maPharmacie = new Pharmacie("adresse",87,44444,"2.5","6.1",new ArrayList<Achat>());
+        Pharmacie maPharmacie = new Pharmacie("adresse",87,44444,"2.5","6.1","maPharmacie",new ArrayList<Achat>());
         maPharmacie.setId(0L);
         Client monClient = new Client("BOUCHER","Pierre","mail@mail.fr","0654478956",new ArrayList<Achat>());
         monClient.setId(0L);
